a9f9b98e21ed0adb1b45c6c3d7a3f2652c01362c
Bichngocc07 <ngoc01072006@gmail.com>
2025-08-12 16:48:06 +0700

    Update Bai04.ts

--- Bai04.ts
+++ Bai04.ts
@@ -1,4 +1,4 @@
-// Định nghĩa lớp Person là abstract class
+
 abstract class Person {
     public name: string;
 
@@ -6,28 +6,27 @@ abstract class Person {
         this.name = name;
     }
 
-    // Phương thức in thông tin (sẽ được ghi đè ở lớp con)
+ 
     displayInfo(): void {
         console.log(`Tên: ${this.name}`);
     }
 }
 
-// Lớp Student kế thừa Person
+
 class Student extends Person {
     public id: string;
 
     constructor(id: string, name: string) {
-        super(name); // gọi constructor lớp cha
+        super(name);
         this.id = id;
     }
 
-    // Ghi đè phương thức displayInfo()
+    
     displayInfo(): void {
         console.log(`Mã SV: ${this.id}, Tên SV: ${this.name}`);
     }
 }
 
-// Lớp Teacher kế thừa Person
 class Teacher extends Person {
     public subject: string;
 
@@ -36,16 +35,17 @@ class Teacher extends Person {
         this.subject = subject;
     }
 
-    // Ghi đè phương thức displayInfo()
+    
     displayInfo(): void {
         console.log(`Tên GV: ${this.name}, Môn dạy: ${this.subject}`);
     }
 }
 
-// Tạo đối tượng Student
-const student = new Student("SV001", "Nguyễn Văn A");
-student.displayInfo(); // Mã SV: SV001, Tên SV: Nguyễn Văn A
 
-// Tạo đối tượng Teacher
+const student = new Student("SV001", "Nguyễn Văn A");
+student.displayInfo(); 
+
+
 const teacher = new Teacher("Trần Thị B", "Toán học");
-teacher.displayInfo(); // Tên GV: Trần Thị B, Môn dạy: Toán học
+teacher.displayInfo();
+
