4db26c82b03407a9372810833c86572ee660e5cb
Bichngocc07 <ngoc01072006@gmail.com>
2025-08-12 16:47:01 +0700

    Update Bai03.ts

--- Bai03.ts
+++ Bai03.ts
@@ -1,41 +1,41 @@
-// Định nghĩa lớp Animal là abstract class
-abstract class Animal {
-    public name: string; // thuộc tính tên động vật
 
+abstract class Animal {
+    public name: string;
     constructor(name: string) {
         this.name = name;
     }
 
-    // Phương thức abstract: các lớp con phải cài đặt lại
+    
     abstract makeNoise(): void;
 
-    // Phương thức in tên động vật
+    
     printName(): void {
         console.log(`Tên: ${this.name}`);
     }
 }
 
-// Lớp Cat kế thừa Animal
+
 class Cat extends Animal {
     makeNoise(): void {
         console.log("meo meo");
     }
 }
 
-// Lớp Dog kế thừa Animal
+
 class Dog extends Animal {
     makeNoise(): void {
         console.log("gâu gâu");
     }
 }
 
-// Tạo đối tượng
+
 const myCat = new Cat("Miu Miu");
 const myDog = new Dog("Cún Con");
 
-// Gọi phương thức
-myCat.printName(); // Tên: Miu Miu
-myCat.makeNoise(); // meo meo
 
-myDog.printName(); // Tên: Cún Con
-myDog.makeNoise(); // gâu gâu
+myCat.printName(); 
+myCat.makeNoise();
+
+myDog.printName();
+myDog.makeNoise(); 
+
